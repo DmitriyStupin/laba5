599c521c5de3fa7a7e5eeecdb59f47a21ddf07c2
Dmitry Stupin <dm.stup@mail.ru>
2024-11-25 11:39:37 +0700

    Добавьте файлы проекта.

---
+++ Model/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        private string _name;
+
+        public string Speciality 
+        {
+            get
+            {
+                return _speciality;
+            }
+            set
+            {
+                _speciality = value;
+                OnPropertyChanged(nameof(Speciality));
+            }
+        }
+        private string _speciality;
+
+        public string Group { get; set; }
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
